ec26e64ebf775ffd19eaeff9a8f276a17b056109
Alina <red.alinka.malinka@gmail.com>
2025-11-17 22:13:07 +0300

    fixed final

--- tasks/redkina_a_min_elem_vec/tests/performance/main.cpp
+++ tasks/redkina_a_min_elem_vec/tests/performance/main.cpp
@@ -2,7 +2,6 @@
 
 #include <cstddef>
 #include <random>
-#include <vector>
 
 #include "redkina_a_min_elem_vec/common/include/common.hpp"
 #include "redkina_a_min_elem_vec/mpi/include/ops_mpi.hpp"
@@ -12,39 +11,35 @@
 namespace redkina_a_min_elem_vec {
 
 class RedkinaAMinElemVecRunPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const size_t kCount_ = 100000000;
-  InType input_data_{};
-  int expected_result_ = 0;
+ public:
+  static constexpr size_t kSize = 100000000;
 
+ protected:
   void SetUp() override {
-    input_data_ = GenerateRandomVector(kCount_);
-    expected_result_ = -2000;
-  }
-
-  static std::vector<int> GenerateRandomVector(size_t size) {
-    std::vector<int> result(size);
     std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dis(-1000, 1000);
+    std::mt19937 g(rd());
+    std::uniform_int_distribution<int> rand(-1000, 1000);
 
-    for (size_t i = 0; i < size; ++i) {
-      result[i] = dis(gen);
+    input_vec_.resize(kSize);
+    for (size_t i = 0; i < kSize; i++) {
+      input_vec_[i] = rand(g);
     }
 
-    std::uniform_int_distribution<size_t> pos_dis(0, size - 1);
-    size_t min_pos = pos_dis(gen);
-    result[min_pos] = -2000;
-
-    return result;
+    expected_res_ = -2000;
+    input_vec_[kSize / 2] = expected_res_;
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return output_data == expected_result_;
+    return expected_res_ == output_data;
   }
 
   InType GetTestInputData() final {
-    return input_data_;
+    return input_vec_;
   }
+
+ private:
+  InType input_vec_;
+  OutType expected_res_{};
 };
 
 TEST_P(RedkinaAMinElemVecRunPerfTests, RunPerfModes) {
