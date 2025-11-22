63b0347100923679021e873e567aac443392b0f6
Alina <red.alinka.malinka@gmail.com>
2025-11-22 16:51:02 +0300

    fixed rank

--- tasks/redkina_a_min_elem_vec/mpi/src/ops_mpi.cpp
+++ tasks/redkina_a_min_elem_vec/mpi/src/ops_mpi.cpp
@@ -31,66 +31,49 @@ bool RedkinaAMinElemVecMPI::RunImpl() {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   int n = 0;
-  std::vector<int> local_vec;
-
   if (rank == 0) {
-    const auto &vec = GetInput();
-    n = static_cast<int>(vec.size());
+    n = static_cast<int>(GetInput().size());
   }
-
   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  int size_l = n / size;
-  int remainder = n % size;
+  int base = n / size;
+  int rem = n % size;
+  int local_size = (rank < rem) ? base + 1 : base;
 
-  int local_size = (rank < remainder) ? size_l + 1 : size_l;
+  std::vector<int> local_vec(local_size);
 
-  int displacement = 0;
-  if (rank < remainder) {
-    displacement = rank * (size_l + 1);
-  } else {
-    displacement = remainder * (size_l + 1) + (rank - remainder) * size_l;
-  }
-
-  local_vec.resize(local_size);
+  std::vector<int> send_counts;
+  std::vector<int> displs;
 
   if (rank == 0) {
-    const auto &vec = GetInput();
-
-    std::vector<int> send_counts(size);
-    std::vector<int> displacements(size);
-
-    for (int i = 0; i < size; ++i) {
-      send_counts[i] = (i < remainder) ? size_l + 1 : size_l;
-      if (i < remainder) {
-        displacements[i] = i * (size_l + 1);
+    send_counts.resize(size);
+    displs.resize(size);
+    for (int i = 0; i < size; i++) {
+      send_counts[i] = (i < rem) ? base + 1 : base;
+      if (i < rem) {
+        displs[i] = i * (base + 1);
       } else {
-        displacements[i] = remainder * (size_l + 1) + (i - remainder) * size_l;
+        displs[i] = rem * (base + 1) + (i - rem) * base;
       }
     }
+  }
 
-    MPI_Scatterv(vec.data(), send_counts.data(), displacements.data(), MPI_INT, local_vec.data(), local_size, MPI_INT,
+  if (rank == 0) {
+    MPI_Scatterv(GetInput().data(), send_counts.data(), displs.data(), MPI_INT, local_vec.data(), local_size, MPI_INT,
                  0, MPI_COMM_WORLD);
   } else {
     MPI_Scatterv(nullptr, nullptr, nullptr, MPI_INT, local_vec.data(), local_size, MPI_INT, 0, MPI_COMM_WORLD);
   }
 
   int min_l = INT_MAX;
-  if (local_size > 0) {
-    for (int val : local_vec) {
-      min_l = std::min(min_l, val);
-    }
-  }
-
-  if (local_size == 0) {
-    min_l = INT_MAX;
+  for (int v : local_vec) {
+    min_l = std::min(min_l, v);
   }
 
   int min_g = 0;
   MPI_Allreduce(&min_l, &min_g, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 
   GetOutput() = min_g;
-
   return true;
 }
 
